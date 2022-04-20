8cc8cd4a664804dd5342f8354e960edf20572364
ruman <mohammedruman1996@gmail.com>
2022-04-20 20:31:14 +0530

    Placement management system of individual module ..

---
+++ Placement/src/main/java/com/tns/placement_management_system/UserRepository.java
@@ -0,0 +1,8 @@
+package com.tns.placement_management_system;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>
+{
+
+}
